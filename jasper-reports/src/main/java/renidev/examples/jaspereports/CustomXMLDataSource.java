0921edb1efa4b223c105dff32f2235d80b095b88
Renan Huanca <renan@huanca.net>
2011-06-11 21:13:20 -0400

    Added CustomXMLDataSource and its test.

--- jasper-reports/src/main/java/renidev/examples/jaspereports/CustomXMLDataSource.java
+++ jasper-reports/src/main/java/renidev/examples/jaspereports/CustomXMLDataSource.java
@@ -1,30 +1,45 @@
 package renidev.examples.jaspereports;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRField;
+import net.sf.jasperreports.engine.JRRewindableDataSource;
 
 /**
  * This is a sample custom data source for xml.
- * @author rhuanca
+ * @author Renan Huanca
  *
  */
-public class CustomXMLDataSource implements JRDataSource{
+public class CustomXMLDataSource implements JRDataSource, JRRewindableDataSource {
 
-	private Document document;
+	private NodeList childNodes;
+	private int index;
 	
 	public CustomXMLDataSource(Document document) {
-		this.document = document;
+		// Note: document.getFirstChild() will return the root node.
+		this.childNodes = document.getFirstChild().getChildNodes();
+		this.index = -1;
 	}
 
+	/**
+	 * Implementation of the getFieldValue method.
+	 * In this case this method is extracting the value 
+	 * from the note's attributes.
+	 */
 	public Object getFieldValue(JRField jrField) throws JRException {
-		return null;
+		String nodeName = jrField.getDescription();
+		return childNodes.item(index).getAttributes().getNamedItem(nodeName).getTextContent();
 	}
 
 	public boolean next() throws JRException {
-		return false;
+		++this.index;
+		return this.index < childNodes.getLength();
 	}
 
+	public void moveFirst() throws JRException {
+		this.index = -1;
+	}
 }
--- jasper-reports/src/test/java/renidev/examples/jasperreports/CustomXMLDataSourceTest.java
+++ jasper-reports/src/test/java/renidev/examples/jasperreports/CustomXMLDataSourceTest.java
@@ -3,7 +3,10 @@ package renidev.examples.jasperreports;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRField;
+import net.sf.jasperreports.engine.design.JRDesignField;
 import renidev.examples.jaspereports.CustomXMLDataSource;
+
 
 public class CustomXMLDataSourceTest {
 	
@@ -12,5 +15,92 @@ public class CustomXMLDataSourceTest {
 		String xml ="<root></root>";
 		CustomXMLDataSource dataSource = new CustomXMLDataSource(SimpleDocumentBuilder.buildDocument(xml));
 		assertFalse(dataSource.next());
+	}
+	
+	@Test
+	public void one_child_node() throws JRException{
+		String xml ="<friends><friend name='carlos'/></friends>";
+		CustomXMLDataSource dataSource = new CustomXMLDataSource(SimpleDocumentBuilder.buildDocument(xml));
+		assertTrue(dataSource.next());
+		assertEquals("carlos", dataSource.getFieldValue(createField("name")));
+		assertFalse(dataSource.next());
+	}
+	
+	@Test
+	public void two_child_nodes() throws JRException{
+		String xml =
+				"<friends>" +
+					"<friend name='carlos'/>" +
+					"<friend name='gonzalo'/>" +
+				"</friends>";
+		CustomXMLDataSource dataSource = new CustomXMLDataSource(SimpleDocumentBuilder.buildDocument(xml));
+		JRField nameField = createField("name");
+		
+		// reading data data
+		assertTrue(dataSource.next());
+		assertEquals("carlos", dataSource.getFieldValue(nameField));
+		assertTrue(dataSource.next());
+		assertEquals("gonzalo", dataSource.getFieldValue(nameField));
+		
+		// checking there is no more data 
+		assertFalse(dataSource.next());
+	}
+	
+	@Test
+	public void two_child_nodes_and_two_attributes() throws JRException{
+		String xml =
+				"<friends>" +
+					"<friend name='carlos' from='work'/>" +
+					"<friend name='gonzalo' from='raquet'/>" +
+				"</friends>";
+		CustomXMLDataSource dataSource = new CustomXMLDataSource(SimpleDocumentBuilder.buildDocument(xml));
+		JRField nameField = createField("name");
+		JRField fromField = createField("from");
+		
+		// reading data data
+		assertTrue(dataSource.next());
+		assertEquals("carlos", dataSource.getFieldValue(nameField));
+		assertEquals("work", dataSource.getFieldValue(fromField));
+		assertTrue(dataSource.next());
+		assertEquals("gonzalo", dataSource.getFieldValue(nameField));
+		assertEquals("raquet", dataSource.getFieldValue(fromField));
+		
+		// checking there is no more data 
+		assertFalse(dataSource.next());
+	}
+
+	
+	@Test
+	public void two_child_nodes_and_rewind() throws JRException{
+		String xml =
+				"<friends>" +
+					"<friend name='carlos'/>" +
+					"<friend name='gonzalo'/>" +
+				"</friends>";
+		
+		CustomXMLDataSource dataSource = new CustomXMLDataSource(SimpleDocumentBuilder.buildDocument(xml));
+		JRField nameField = createField("name");
+		
+		// bypassing two nodes
+		assertTrue(dataSource.next());
+		assertTrue(dataSource.next());
+		
+		// checking there is no more data 
+		assertFalse(dataSource.next());
+		
+		// rewind the data source
+		dataSource.moveFirst();
+		
+		// reading data data
+		assertTrue(dataSource.next());
+		assertEquals("carlos", dataSource.getFieldValue(nameField));
+		assertTrue(dataSource.next());
+		assertEquals("gonzalo", dataSource.getFieldValue(nameField));
+	}
+	
+	public JRField createField(String description){
+		JRDesignField field = new JRDesignField();
+		field.setDescription(description);
+		return field;
 	}
 }
