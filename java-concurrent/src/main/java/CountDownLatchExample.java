f57678c4f80fe43d038e1d17320395e2abfef5b8
Renan Huanca <renan@huanca.net>
2011-06-25 21:44:25 -0400

    Added CountDownLatchExample class.

---
+++ java-concurrent/src/main/java/CountDownLatchExample.java
@@ -0,0 +1,44 @@
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchExample {
+	
+	public static void main(String... args) {
+		// Instantiating countdownlatch class.
+		CountDownLatch countDown = new CountDownLatch(1);
+		System.out.println("Creating threads...");
+		// going to create 50 threads.
+		for(int i=0;i<50;i++) {
+			Worker worker = new Worker(countDown, "Worker #"+i);
+			new Thread(worker).start();
+		}
+		
+		// Now start all threads at the same time.
+		countDown.countDown();
+		
+	}
+	
+	/**
+	 * This class is just a simple dummy thread.
+	 *
+	 */
+	private static class  Worker implements Runnable {
+		CountDownLatch startLatch;
+		String name;
+		
+		public Worker(CountDownLatch startLatch, String name) {
+			super();
+			this.startLatch = startLatch;
+			this.name = name;
+		}
+
+		public void run() {
+			try {
+				this.startLatch.await();
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+			
+			System.out.println("Running " + name+".");
+		}
+	}
+}
